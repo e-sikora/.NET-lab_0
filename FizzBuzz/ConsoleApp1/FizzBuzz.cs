40db54157176a1dce35cb5f9f476fcc5cb9f6648
e-sikora <106074264+e-sikora@users.noreply.github.com>
2024-02-28 18:48:09 +0100

    Add files via upload

---
+++ FizzBuzz/ConsoleApp1/FizzBuzz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class FizzBuzz
+    {
+
+        int upperBound = 10;
+
+
+        public FizzBuzz(int upper)
+        {
+            upperBound = upper;
+        }
+        public void Dispaly()
+        {
+            for (int i = 1; i < upperBound; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 ==0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else { 
+                    Console.WriteLine("Fizz");
+                     }
+                }
+                else if (i % 5 == 0) 
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
---
+++ FizzBuzz/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fz1 = new FizzBuzz(60);
+
+            fz1.Dispaly();
+        }
+    }
+}
